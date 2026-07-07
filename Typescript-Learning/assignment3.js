4fd3f2110bd44fd798555bb72109c0eb13205eac
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2026-07-07 11:53:28 +0530

    Added assignment3

---
+++ Typescript-Learning/assignment3.js
@@ -0,0 +1,9 @@
+"use strict";
+const bankName = "Bank Of Maharshtra ";
+const accNumber = 1234567890;
+let balance = 340000;
+let isActive = true;
+console.log(bankName);
+console.log(accNumber);
+console.log(balance);
+console.log(isActive);
---
+++ Typescript-Learning/assignment3.ts
@@ -0,0 +1,9 @@
+const bankName:String = "Bank Of Maharshtra ";
+const accNumber:Number= 1234567890;
+let balance :Number=340000;
+let isActive:boolean=true;
+
+console.log(bankName);
+console.log(accNumber);
+console.log(balance);
+console.log(isActive);

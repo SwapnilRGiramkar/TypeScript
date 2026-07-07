e479fc23765101de74b95b35c64f4d07be897535
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2026-07-07 11:48:41 +0530

    Addded Assignment2

---
+++ Typescript-Learning/assignment2.js
@@ -0,0 +1,9 @@
+"use strict";
+const prodName = "Iphone 17";
+let prodPrice = 120000;
+let prodQuntity = 23;
+let isAvailable = true;
+console.log(prodName);
+console.log(prodPrice);
+console.log(prodQuntity);
+console.log(isAvailable);
---
+++ Typescript-Learning/assignment2.ts
@@ -0,0 +1,9 @@
+const prodName:String="Iphone 17";
+let prodPrice:number=120000;
+let prodQuntity:number=23;
+let isAvailable:boolean=true;
+
+console.log(prodName);
+console.log(prodPrice);
+console.log(prodQuntity);
+console.log(isAvailable);

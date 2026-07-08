32a1b91b13e1dea5eb649a46c86a60ad1e33655f
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2026-07-08 12:30:26 +0530

    Null Type

---
+++ Typescript-Learning/nulltype.js
@@ -0,0 +1,7 @@
+"use strict";
+//null type
+let manager = null;
+manager = "Swapnil Giramkar";
+console.log(manager);
+manager = null;
+console.log(manager);
---
+++ Typescript-Learning/nulltype.ts
@@ -0,0 +1,9 @@
+//null type
+let manager:string | null = null;
+manager = "Swapnil Giramkar";
+
+console.log(manager);
+
+manager=null;
+
+console.log(manager);

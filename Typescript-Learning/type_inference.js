1b0ead6b422f61127f8ff6b5d7f9c5e8b44c32d9
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2026-07-08 12:09:28 +0530

    Type Inference

---
+++ Typescript-Learning/type_inference.js
@@ -0,0 +1,19 @@
+"use strict";
+//This is type inference just similar working as type annotation just like type annotation we can also use type inference in typescript. 
+// In type inference we don't need to specify the data type of variable explicitly, 
+// typescript will automatically infer the data type of variable based on the value assigned to it.
+let myName = "Swpnil Giramkar";
+let city = "Pune";
+let state = "Maharashtra";
+let country = "India";
+let age = 24;
+let isMarried = false;
+let isWorking = true;
+console.log("\n********* Personal Information *********\n");
+console.log("Name    : " + myName);
+console.log("City    : " + city);
+console.log("State   : " + state);
+console.log("Country : " + country);
+console.log("Age     : " + age);
+console.log("Married : " + isMarried);
+console.log("Working : " + isWorking);

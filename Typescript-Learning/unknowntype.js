91416c7b05be03b1a36c960740d4d5419bf67edd
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2026-07-08 12:25:37 +0530

    unknown type

---
+++ Typescript-Learning/unknowntype.js
@@ -0,0 +1,11 @@
+"use strict";
+// unknown type 
+let value;
+value = "Swapnil";
+value = 23;
+value = false;
+console.log(value);
+let data = "typescript";
+if (typeof data === "string") {
+    console.log(data.toUpperCase());
+}
---
+++ Typescript-Learning/unknowntype.ts
@@ -0,0 +1,13 @@
+// unknown type 
+let value: unknown;
+value="Swapnil";
+value =23;
+value=false;
+
+console.log(value);
+
+let data:unknown ="typescript";
+if(typeof data === "string")
+{
+    console.log(data.toUpperCase());    
+}

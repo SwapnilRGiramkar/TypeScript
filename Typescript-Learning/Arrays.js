297b79aea0084aca433d69eb2b46388bc888f899
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2026-07-09 12:50:55 +0530

    Arrays Basics

---
+++ Typescript-Learning/Arrays.js
@@ -0,0 +1,7 @@
+"use strict";
+//Numbers Array
+let numbers = [10, 20, 50, 30, 49];
+console.log(numbers);
+//String Array
+let fruits = ["Mango", "Banana", "Apple", "Grapes"];
+console.log(fruits);
---
+++ Typescript-Learning/Arrays.ts
@@ -0,0 +1,6 @@
+//Numbers Array
+let numbers: number[] =[10,20,50,30,49];
+console.log(numbers);
+//String Array
+let fruits: string[] = ["Mango","Banana","Apple","Grapes"];
+console.log(fruits);

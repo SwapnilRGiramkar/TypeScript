263c155fdfc76f9dc047f839189335417f8c31f6
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2026-07-09 12:56:38 +0530

    Types Of Arrays

--- Typescript-Learning/Arrays.js
+++ Typescript-Learning/Arrays.js
@@ -5,3 +5,12 @@ console.log(numbers);
 //String Array
 let fruits = ["Mango", "Banana", "Apple", "Grapes"];
 console.log(fruits);
+// Boolean Array 
+let decision = [true, false, true];
+console.log(decision);
+//Mixed Array
+let data = ["Swapnil", 24, "Pune", 411009];
+console.log(data);
+//Array<type>
+let marks = [23, 78, 99, 80, 67];
+console.log(marks);
--- Typescript-Learning/Arrays.ts
+++ Typescript-Learning/Arrays.ts
@@ -4,3 +4,13 @@ console.log(numbers);
 //String Array
 let fruits: string[] = ["Mango","Banana","Apple","Grapes"];
 console.log(fruits);
+// Boolean Array 
+let decision : boolean[]=[true,false,true];
+console.log(decision);
+//Mixed Array
+let data : (string|number)[]=["Swapnil",24,"Pune",411009];
+console.log(data);
+//Array<type>
+let marks : Array<number>=[23,78,99,80,67];
+console.log(marks);
+

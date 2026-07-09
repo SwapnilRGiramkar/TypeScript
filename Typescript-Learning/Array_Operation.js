12cc5d16adc7cecf614f90ac8341dd34f6434646
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2026-07-09 13:02:00 +0530

    Performing Operrations on Array

---
+++ Typescript-Learning/Array_Operation.js
@@ -0,0 +1,13 @@
+"use strict";
+//Accessing Array Elements 
+let colors = ["Red", "Green", "Blue", "Yellow"];
+console.log(colors[1]);
+console.log(colors[3]);
+console.log(colors[0]);
+// adding elements to an array 
+colors.push("Pink");
+colors.push("Black");
+console.log(colors);
+//Removing last Element from an array
+colors.pop();
+console.log(colors);
---
+++ Typescript-Learning/Array_Operation.ts
@@ -0,0 +1,14 @@
+//Accessing Array Elements 
+let colors: string[]=["Red","Green","Blue","Yellow"];
+console.log(colors[1]);
+console.log(colors[3]);
+console.log(colors[0]);
+
+// adding elements to an array 
+colors.push("Pink");
+colors.push("Black");
+console.log(colors);
+
+//Removing last Element from an array
+colors.pop();
+console.log(colors);

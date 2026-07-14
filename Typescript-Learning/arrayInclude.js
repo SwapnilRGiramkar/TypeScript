4bfbbf3142d6558715e570c0788ac3bf2dffff5c
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2026-07-14 12:16:19 +0530

    includes() examples of an array function

--- Typescript-Learning/arrayInclude.js
+++ Typescript-Learning/arrayInclude.js
@@ -2,3 +2,11 @@
 let roll = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(roll.includes(5)); //true
 console.log(roll.includes(15)); //false
+let colors = ["red", "blue", "yelw", "black", "violet"];
+console.log(colors.includes("orange"));
+if (colors.includes("blue")) {
+    console.log("Color is present in the array");
+}
+else {
+    console.log("Color is not present in the array");
+}
--- Typescript-Learning/arrayInclude.ts
+++ Typescript-Learning/arrayInclude.ts
@@ -1,3 +1,14 @@
 let roll:number[]=[1,2,3,4,5,6,7,8,9,10];
 console.log(roll.includes(5)); //true
 console.log(roll.includes(15)); //false
+
+let colors:string[]=["red","blue","yelw","black","violet"];
+console.log(colors.includes("orange"));
+if(colors.includes("blue"))
+{
+    console.log("Color is present in the array");
+}
+else
+{
+    console.log("Color is not present in the array");
+}
